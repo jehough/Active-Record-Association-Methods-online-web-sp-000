806fd6366b8515129b2540c2282253b326a6e515
Jim Hough <teachlife@outlook.com>
2019-06-04 00:34:37 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.map { |e| e.name }
+    self.artists.map { |e| e.name }
   end
 end
